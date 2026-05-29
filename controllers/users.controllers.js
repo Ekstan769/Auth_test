63f591ff94341356622bc27915bbc3e0fec4eb81
Eke Stanley <ekestanley1234@gmail.com>
2026-05-29 23:22:49 +0100

    initial commit

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.models");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 const registerUser = async (req, res) => {
@@ -16,7 +17,15 @@ const registerUser = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({ firstName, lastName, email, password: hashedPassword });
-        return res.status(201).json({ message: "User created successfully", user: newUser });
+        
+        // syntax not to return users password
+        const userResponse = {
+            id: newUser. _id,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            email: newUser.email,
+        };
+        return res.status(201).json({ message: "User created successfully", user: newResponse });
     } catch(e) {
         console.log(e);
         
@@ -41,10 +50,67 @@ const signIn = async (req, res) => {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
-        return res.status(200).json({ message: "User signed in successfully", user });
+        const userResponse = {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            token: token,
+        };
+
+        const token = jwt.sign({ id: user._id, firstName: user.firstName }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+        })
+
+        return res.status(200).json({ message: "User signed in successfully", user: userResponse });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+// syntax to make user an admin/moderator
+const newAdmin = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        user.role = "admin";
+        await user.save();
+        return res.status(200).json({ message: "User role updated to admin" });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+const newModerator = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        user.role = "moderator";
+        await user.save();
+        return res.status(200).json({ message: "User role updated to moderator" });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({ message: "You are not authorized to perform this action" });
+    }
+    try {
+        const users = await User.find().select("-password");
+        return res.status(200)({ users });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: "Internal server error"})
     }
 };
 
@@ -52,4 +118,7 @@ const signIn = async (req, res) => {
 module.exports = {
     registerUser,
     signIn, 
+    newAdmin,
+    newModerator,
+    getAllUsers,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { registerUser, signIn }  = require('../controllers/users.controllers');
+const { registerUser, signIn, newAdmin, newModerator, getAllUsers }  = require('../controllers/users.controllers');
+const isAuthentication = require("../utils/isAuthentication");
 
 const router = require("express").Router();
 
 router.post("/register", registerUser);
 router.post("/signin", signIn);
-
-
+router.patch("/new-admin/:userId", newAdmin);
+router.patch("/new-moderator/:userId", newModerator);
+router.get("/all-users", isAuthentication, getAllUsers);
 
 
 module.exports = router;
